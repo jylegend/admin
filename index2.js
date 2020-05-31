ba6b670d5c6ea3ef49ced45ae8edb36eebb8b44c
jy912 <787113172@qq.com>
2020-05-31 09:26:43 +0800

    更新

--- index2.js
+++
@@ -1,1 +0,0 @@
-module.exports = "It works from index2.js.";
--- js/sub.js
+++
@@ -1,9 +0,0 @@
-function generateText(){
-	var element=document.createElement('h2');
-
-	element.innerHTML="Hello1 Webpack !!!!";
-
-	return element;
-}
-
-module.exports=generateText;
